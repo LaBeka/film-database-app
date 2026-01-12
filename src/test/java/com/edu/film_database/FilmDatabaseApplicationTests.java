8c1135ca994a5edd9a4dcb7ade9065e1de4edb38
Begaiym Kalilova <easybeka@gmail.com>
2026-01-12 16:00:18 +0100

    first commit

---
+++ src/test/java/com/edu/film_database/FilmDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.film_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
